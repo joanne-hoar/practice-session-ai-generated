813c01a6eabc038aa26a507de73e84ebbf56617d
Jo Hoar <joanne.hoar@robertson.brightspace.com>
2026-02-04 14:06:49 -0700

    Update app title to 'Everyday Market App'

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('practice-session');
+  protected readonly title = signal('Everyday Market App');
 }
